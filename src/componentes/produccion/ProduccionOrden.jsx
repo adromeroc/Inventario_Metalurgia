b2226a8ab6522815d7ed39283f457e9bf59fc553
Deyver Velez <Deyverv@uninorte.edu.co>
2021-11-29 17:55:12 -0500

    Pantallas Orden y Solicitud de produccion

--- src/componentes/produccion/ProduccionOrden.jsx
+++ src/componentes/produccion/ProduccionOrden.jsx
@@ -4,8 +4,66 @@ import DefaultContentMensaje from "../DefaultContentMensaje";
 export default function ProduccionOrden() {
     return (
         <Fragment>
-			{/* Elimina el siguiente tag y coloca lo que necesites */}
-            <DefaultContentMensaje msg="Orden de producción" />
+            <div className="container d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h2>Crear Orden de Producción</h2>
+                <form className="d-flex" action="" method="POST">
+                    <input type="text" className="form-control me-2" name="" placeholder="Buscar Productos" />
+                    <button className="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
+                <div></div>
+            </div>
+
+			<div className="container pt-3 pb-2 mb-3">
+                <div className="row">
+                    <div className="table-responsive pt-3 pb-2 mb-3">
+                    <table className="table table-striped table-sm">
+                        <thead>
+                        <tr>
+                            <th scope="col">Productos</th>
+                            <th scope="col">Cantidad a Producir</th>
+                            <th scope="col" className="text-center">Acción</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Producto 1</td>
+                                <td>
+                                    <input type="number" className="form-control me-6" name="cantProductos" placeholder="Numero de productos a producir" />
+                                </td>
+                                <td>
+                                    <form className="text-center" action="" method="post">
+                                        <input type="submit" value="Solicitar" class="btn btn-primary" />
+                                    </form>
+                                </td>
+                            </tr>
+                            <tr>
+                            <td>Producto 2</td>
+                                <td>
+                                    <input type="number" className="form-control me-6" name="cantProductos" placeholder="Numero de productos a producir" />
+                                </td>
+                                <td>
+                                    <form className="text-center" action="" method="post">
+                                        <input type="submit" value="Solicitar" class="btn btn-primary" />
+                                    </form>
+                                </td>
+                            </tr>
+                            <tr>
+                            <td>Producto 3</td>
+                                <td>
+                                    <input type="number" className="form-control me-6" name="cantProductos" placeholder="Numero de productos a producir" />
+                                </td>
+                                <td>
+                                    <form className="text-center" action="" method="post">
+                                        <input type="submit" value="Solicitar" class="btn btn-primary" />
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+                </div>
+                
         </Fragment>
     )
 }
--- src/componentes/produccion/ProduccionSolicitud.jsx
+++ src/componentes/produccion/ProduccionSolicitud.jsx
@@ -4,8 +4,51 @@ import DefaultContentMensaje from "../DefaultContentMensaje";
 export default function ProduccionSolicitud() {
     return (
         <Fragment>
-			{/* Elimina el siguiente tag y coloca lo que necesites */}
-            <DefaultContentMensaje msg="Solicitud de producción" />
+			<div className="container pt-3 pb-2 mb-3">
+                <div className="row">
+                    <h2>Solicitud de Producción</h2>
+                    <div className="table-responsive pt-3 pb-2 mb-3">
+                    <table className="table table-striped table-sm text-center">
+                        <thead>
+                        <tr>
+                            <th scope="col">Productos</th>
+                            <th scope="col">Cantidades Solicitadas</th>
+                            <th scope="col" className="text-center">Acción</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Producto 1</td>
+                                <td>150</td>
+                                <td>
+                                    <form className="text-center" action="" method="post">
+                                        <input type="submit" value="Producido" class="btn btn-success" />
+                                    </form>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Producto 2</td>
+                                <td>2000</td>
+                                <td>
+                                    <form className="text-center" action="" method="post">
+                                        <input type="submit" value="Producido" class="btn btn-success" />
+                                    </form>
+                                </td>
+                            </tr>
+                            <tr>
+                            <td>Producto 3</td>
+                                <td>5000</td>
+                                <td>
+                                    <form className="text-center" action="" method="post">
+                                        <input type="submit" value="Producido" class="btn btn-success" />
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+                </div>
         </Fragment>
     )
 }
