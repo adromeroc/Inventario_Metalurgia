a76706a69faf4f6981adaba9c26ef77dc8db1653
Conny1978 <92700781+Conny1978@users.noreply.github.com>
2021-11-30 14:08:18 -0500

    Código de registro de usuario
    Código de registro y gestión de usuarios

--- src/componentes/usuario/UsuarioGestion.jsx
+++ src/componentes/usuario/UsuarioGestion.jsx
@@ -4,8 +4,94 @@ import DefaultContentMensaje from "../DefaultContentMensaje";
 export default function UsuarioGestion() {
     return (
         <Fragment>
-            {/* Elimina el siguiente tag y coloca lo que necesites */}
-            <DefaultContentMensaje msg="Gestión Usuarios" />
+            <div className="container">
+                <div className="row align-items-start"></div>
+                    <div className="col">
+                        <div className="mb-3">
+                        <br></br>
+                        <br></br>
+                            <h5>Información personal</h5>
+                            <br></br>
+                        <div className="mb-3">
+                            <label for="sexi" className="form-label">Sexo:</label>
+                        </div>
+                        </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                                <label className="form-check-label" for="inlineRadio1">Masculino</label>
+                            </div>
+                            <div className="form-check form-check-inline">
+                                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                                <label className="form-check-label" for="inlineRadio2">Femenino</label>
+                            </div>
+                            <br></br>
+                            <br></br>
+                            <div className="mb-3">
+                                <label for="identificacion" className="form-label">N° Identificación:</label>
+                                <input type="number" className="form-control" id="identificacion" placeholder="Numero de Identificación"/>
+                            </div>
+                        <div className="mb-3">
+                            <label for="nombres" className="form-label">Nombres:</label>
+                            <input type="text" className="form-control" id="nombres" placeholder="Ingrese nombres"required=""/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="apellidos" className="form-label">Apellidos:</label>
+                            <input type="text" className="form-control" id="apellidos" placeholder="Ingrese apellidos"required=""/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="fechaNacimiento" className="form-label">Fecha de nacimiento:</label>
+                            <input type="date" className="form-control" id="fechaNacimiento" placeholder="DD / MM / AAAA"required=""/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="celular" className="form-label">Telefono Fijo/Celular:</label>
+                            <input type="number" className="form-control" id="celular" placeholder="Ingrese número"required=""/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="direccion" className="form-label">Dirección:</label>
+                            <input type="address" className="form-control" id="direccion" placeholder="Ingrese dirección"required=""/>
+                        </div>
+                    
+                        <div className="mb-3">
+                            <label for="correoElectronico" className="form-label">Correo electrónico Personal</label>
+                            <input type="email" className="form-control" id="correoElectronico" placeholder="Ingrese dirección de correo electrónico."required=""/>
+                        </div>
+                        <div className="u-form-group u-form-message u-form-group-7">
+                            <label for="otraInformacion" className="form-label">Informacion Adicional:</label>
+                            <textarea placeholder="Ingrese información adicional" rows="4" cols="50" id="otraInformacion" name="message-1" className="form-control"></textarea>
+                            <br></br>
+                            <br></br>
+                        <form action="">
+                        <div>
+                        <h5>Datos para ingreso al Sistema</h5>
+                        <br></br>
+                            <div className="input-group input-group-sm mb-3">
+                                <span className="input-group-text" id="inputGroup-sizing-sm">Usuario</span>
+                                <input className="form-control" id="user" placeholder="Cree un usuario para ingresar al sistema"/>
+                            </div>
+                            <div className="input-group input-group-sm mb-3">
+                                <span className="input-group-text" id="inputGroup-sizing-sm">Contraseña</span>
+                                <input className="form-control" id="contrasena" placeholder="Asignele una conraseña al usuario registrado"/>
+                            </div>    
+                        </div>
+                        <div className="mb-3"></div>
+                        <label for="sexi" className="form-label">Perfil del Usuario:</label>
+                        <br></br>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                            <label className="form-check-label" for="inlineRadio1">Administrador</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                            <label className="form-check-label" for="inlineRadio2">Usuario de Producción </label>
+                        </div> 
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                            <label className="form-check-label" for="inlineRadio2">Usuario de Inventarios</label>
+                        </div> 
+                        </form>   
+                        </div>   
+                    </div>      
+                </div>
         </Fragment>
-    )
+    );
 }
--- src/componentes/usuario/UsuarioRegistro.jsx
+++ src/componentes/usuario/UsuarioRegistro.jsx
@@ -1,11 +1,44 @@
 import { Fragment } from "react";
 import DefaultContentMensaje from "../DefaultContentMensaje";
+// import logo from '../image/Foto.jpg';
 
 export default function UsuarioRegistro() {
     return (
         <Fragment>
-            {/* Elimina el siguiente tag y coloca lo que necesites */}
-            <DefaultContentMensaje msg="Registro Usuarios" />
+            <div className="container">
+                {/* <div>
+                    <img className="mb-4" alt="" width="140" height="100" src={logo} />
+                </div> */}
+                <div className="text-center">
+                    <br></br>
+                    <br></br>
+                    <h3>Iniciar sesión</h3>
+                    <br></br>
+                </div>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" className="w-50 form-control" placeholder="Ingresar email"/>
+                    </div>
+                    <div className="form-group">
+                        <label>Contraseña</label>
+                        <input type="password" className="w-50 form-control" placeholder="Ingresar password"/>
+                    </div>
+                    <br></br>
+                    <div className="text-center">
+                    <div className="form-group">
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                            <label className="custom-control-label" htmlFor="customCheck1"> Recordarme</label>
+                        </div>
+                        <br></br>
+                        <button type="submit" className="w-50 btn btn-lg btn-primary">Entrar</button>
+                        <p className="forgot-password text-right">
+                        </p>
+                    </div>
+                    </div>
+                    
+                 
+            </div>
         </Fragment>
     )
 }
