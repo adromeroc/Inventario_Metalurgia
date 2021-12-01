87e61e4343ae1a2ed6f6142eaa61d1a5dc1340e1
Pablo Rodriguez <pmonteroa@uninorte.edu.co>
2021-11-30 21:08:04 -0500

    Pantallas de producto
    -Se realiza la pantalla para crear producto
    .Se crea la pantalla para listar los productos

--- src/componentes/producto/ProductoCrear.jsx
+++ src/componentes/producto/ProductoCrear.jsx
@@ -4,8 +4,47 @@ import DefaultContentMensaje from "../DefaultContentMensaje";
 export default function ProductoCrear() {
     return (
         <Fragment>
-            {/* Elimina el siguiente tag y coloca lo que necesites */}
-            <DefaultContentMensaje msg="Crear productos" />
+            <div className="container d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h2>Crear Producto</h2>
+                <div></div>
+            </div>
+            <div>
+                <form>
+                    <div className="form-group row">
+                        <label htmlFor="inputIdProducto" className="col-sm-2 col-form-label">Id</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="inputIdProducto" placeholder="Identificador producto" />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="inputNombre" className="col-sm-2 col-form-label">Nombre</label>
+                        <div className="col-sm-10">
+                            <input type="text" className="form-control" id="inputNombre" placeholder="Nombre del producto" />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="inputUnidadMedida" className="col-sm-2 col-form-label">Unidad Medida</label>
+                        <div className="col-sm-10">
+                            <select id="inputUnidadMedida" class="form-control">
+                                <option selected>- Seleccione -</option>
+                                <option>CM (Centimetro)</option>
+                                <option>M (Metro)</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <label htmlFor="inputValor" className="col-sm-2 col-form-label">Valor</label>
+                        <div className="col-sm-10">
+                            <input type="number" className="form-control" id="inputValor" placeholder="Valor del producto" />
+                        </div>
+                    </div>
+                    <div className="form-group row">
+                        <div className="col-sm-10">
+                            <button type="submit" className="btn btn-primary">Crear Producto</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </Fragment>
     )
 }
--- src/componentes/producto/ProductoLista.jsx
+++ src/componentes/producto/ProductoLista.jsx
@@ -4,8 +4,70 @@ import DefaultContentMensaje from "../DefaultContentMensaje";
 export default function ProductoLista() {
     return (
         <Fragment>
-            {/* Elimina el siguiente tag y coloca lo que necesites */}
-            <DefaultContentMensaje msg="Lista de productos" />
+            <div className="container d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h2>Listar Producto</h2>
+                <form className="d-flex" action="" method="POST">
+                    <input type="text" className="form-control me-2" name="" placeholder="Buscar Productos" />
+                    <button className="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
+                <div></div>
+            </div>
+            <div className="container pt-3 pb-2 mb-3">
+                <div className="row">
+                    <div className="table-responsive pt-3 pb-2 mb-3">
+                        <table className="table table-striped table-sm text-center">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Valor</th>
+                                    <th scope="col">Acción</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Producto 1</td>
+                                    <td>$250.000</td>
+                                    <td>
+                                        <form className="text-center" action="" method="post">
+                                            <input type="submit" value="Editar Producto" className="btn btn-success" />
+                                            &nbsp;
+                                            <input type="submit" value="Agregar Materia" className="btn btn-success" />
+                                            &nbsp;
+                                            <input type="submit" value="Eliminar Producto" className="btn btn-danger" />
+                                        </form>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Producto 2</td>
+                                    <td>$320.000</td>
+                                    <td>
+                                        <form className="text-center" action="" method="post">
+                                            <input type="submit" value="Editar Producto" className="btn btn-success" />
+                                            &nbsp;
+                                            <input type="submit" value="Agregar Materia" className="btn btn-success" />
+                                            &nbsp;
+                                            <input type="submit" value="Eliminar Producto" className="btn btn-danger" />
+                                        </form>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Producto 3</td>
+                                    <td>$125.000</td>
+                                    <td>
+                                        <form className="text-center" action="" method="post">
+                                            <input type="submit" value="Editar Producto" className="btn btn-success" />
+                                            &nbsp;
+                                            <input type="submit" value="Agregar Materia" className="btn btn-success" />
+                                            &nbsp;
+                                            <input type="submit" value="Eliminar Producto" className="btn btn-danger" />
+                                        </form>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     )
 }
