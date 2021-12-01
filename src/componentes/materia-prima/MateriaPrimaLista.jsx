7aa7bb2fe2ff7385f25c98d26f5b07bb4ef3adc0
Andrea <adcastaneda@uninorte.edu.co>
2021-11-30 19:19:59 -0500

    MP Lista

--- src/componentes/materia-prima/MateriaPrimaLista.jsx
+++ src/componentes/materia-prima/MateriaPrimaLista.jsx
@@ -4,8 +4,76 @@ import DefaultContentMensaje from "../DefaultContentMensaje";
 export default function MateriaPrimaLista() {
     return (
         <Fragment>
-            {/* Elimina el siguiente tag y coloca lo que necesites */}
-            <DefaultContentMensaje msg="Lista de materias primas" />
+           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 className="h2">Listar Materia Prima</h1>
+          <form className="d-flex" action="/prueba" method="POST">
+            <input type="text" className="form-control me-2" name="buscarEvaluacion" placeholder="Referencia" />
+            <button className="btn btn-sm btn-outline-secondary" type="submit">Buscar</button>
+          </form>
+          
+        </div>
+        
+        <div className="table-responsive">
+          <table className="table table-striped table-sm">
+            <thead>
+              <tr>
+                <th scope="col">Descripción</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Valor</th>
+                <th scope="col">Acciones</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Acero al Carbón</td>
+                <td>20 TON</td>
+                <td>$20.000</td>
+                <td>
+                  <form action="" method="post">
+                    <input type="submit" value="Editar" className="btn btn-primary" />
+                  </form>
+                </td>
+                <td>
+                  <form action="" method="post">
+                    <input type="submit" value="Eliminar" className="btn btn-danger" />
+                  </form>
+                </td>
+              </tr>
+              <tr>
+                <td>Hierro Fundido</td>
+                <td>3 TON</td>
+                <td>$30.000</td>
+                <td>
+                  <form action="" method="post">
+                    <input type="submit" value="Editar" className="btn btn-primary" />
+                  </form>
+                </td>
+                <td>
+                  <form action="" method="post">
+                    <input type="submit" value="Eliminar" className="btn btn-danger" />
+                  </form>
+                </td>
+              </tr>
+              <tr>
+                <td>Acero Inoxidable</td>
+                <td>4 TON</td>
+                <td>$40.000</td>
+                <td>
+                  <form action="" method="post">
+                    <input type="submit" value="Editar" className="btn btn-primary" />
+                  </form>
+                </td>
+                <td>
+                  <form action="" method="post">
+                    <input type="submit" value="Eliminar" className="btn btn-danger" />
+                  </form>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         </Fragment>
     )
 }
