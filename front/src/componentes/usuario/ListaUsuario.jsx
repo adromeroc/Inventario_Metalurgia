8582ce111bc26ef7663d7e522ef112327a346281
Conny1978 <92700781+Conny1978@users.noreply.github.com>
2021-12-21 12:07:52 -0500

    Guardar usuario front

--- front/src/componentes/usuario/ListaUsuario.jsx
+++ front/src/componentes/usuario/ListaUsuario.jsx
@@ -1,14 +1,49 @@
+import { useEffect, useState } from "react";
 import { Fragment } from "react";
-//import DefaultContentMensaje from "../DefaultContentMensaje";
+
 
 export default function ListaUsuario() {
+    const [listado, setListado] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:8080/usuario/listar", {
+            method: "GET"
+        }).then(res => res.json())
+            .then(res => {
+                if (res.estado === "ok")
+                    setListado(res.data);
+            })
+    }, [])
+
+    // function consultarUsuario() { //Funcion para guardar los usuario
+            
+    //     const ideUsu = ideUsuRef.current.value; /** Almacena en unas variables los datos de las cajas de texto */
+        
+    //     fetch("http://localhost:8080/usuario/consutar", {
+    //         headers: { "content-type": "application/json" },
+    //         method: "POST",
+    //         body: JSON.stringify({ "numero_identificacion": ideUsu })
+    //     }).then(res => res.json())
+    //         .then(res => {
+    //             if (res.estado === "ok")
+    //             nidUsuRef.current.value = res.data._id;
+    //             nomUsuRef.current.value = res.data.nombres;
+    //             apeUsuRef.current.value = res.data.apellidos;
+    //             rolUsuRef.current.value = res.data.rol;
+                
+                
+    //         })
+    // }
+    
+
     return (
         <Fragment>
             <div className="container d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h2>Lista de Usuarios</h2>
                 <form className="d-flex" action="" method="POST">
                     <input type="text" className="form-control me-2" name="" placeholder="Buscar Usuarios" />
+                    {/* <input ref={ideUsuRef} type="text" className="form-control me-2" name="" placeholder="Buscar Usuarios" /> */}
                     <button className="btn btn-outline-success" type="submit">Buscar</button>
+                    {/* <button className="btn btn-primary" type="button" onClick = {consultarUsuario}>Buscar</button> <a> </a> */}
                 </form>
                 <div></div>
             </div>
@@ -19,7 +54,7 @@ export default function ListaUsuario() {
                     <table className="table table-striped table-sm text-center">
                         <thead>
                         <tr>
-                            <th scope="col">ID</th>
+                            <th scope="col">No. Identificación</th>
                             <th scope="col">Nombres</th>
                             <th scope="col">Apellidos</th>
                             <th scope="col">Rol</th>
@@ -28,23 +63,25 @@ export default function ListaUsuario() {
                         </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>111</td>
-                                <td>Nombre Usuario</td>
-                                <td>Apellidos de Usuario</td>
-                                <td>Rol del Usuario</td>
-                                <td>
-                                    <form className="text-center" action="" method="post">
-                                        <input type="submit" value="Editar" className="btn btn-primary" />
-                                    </form>
-                                </td>
-                                <td>
-                                    <form className="text-center" action="" method="post">
-                                        <input type="submit" value="Eliminar" className="btn btn-danger" />
-                                    </form>
-                                </td>
-                            </tr>
-                                                    
+                        {
+                            listado.map(p => <tr>
+                                <td>{p.numero_identificacion}</td>
+                                <td>{p.nombres}</td>
+                                <td>{p.apellidos}</td>
+                                <td>{p.rol}</td>
+                            <td>
+                                <form className="text-center" action="" method="post">
+                                    <input type="submit" value="Editar" className="btn btn-primary" />
+                                </form>
+                            </td>
+                            <td>
+                                <form className="text-center" action="" method="post">
+                                    <input type="submit" value="Eliminar" className="btn btn-danger" />
+                                </form>
+                            </td>
+                            </tr>)
+                        }
+                                                                
                         </tbody>
                     </table>
                     </div>
--- front/src/componentes/usuario/UsuarioGestion.jsx
+++ front/src/componentes/usuario/UsuarioGestion.jsx
@@ -1,13 +1,46 @@
+import { useRef } from "react";
 import { Fragment } from "react";
 //import DefaultContentMensaje from "../DefaultContentMensaje";
-
+    
 export default function UsuarioGestion() {
+    /**Declaracion de variables de referencia */
+    const nidUsuRef = useRef();
+    const nomUsuRef = useRef();
+    const apeUsuRef = useRef();
+    const usuUsuRef = useRef();
+    const conUsuRef = useRef();
+    const rolUsuRef = useRef();
+    
+    
+
+    function guardarUsuario() { //Funcion para guardar los usuario
+            
+        const nidUsu = nidUsuRef.current.value; /** Almacena en unas variables los datos de las cajas de texto */
+        const nomUsu = nomUsuRef.current.value;
+        const apeUsu = apeUsuRef.current.value;
+        const usuUsu = usuUsuRef.current.value;
+        const conUsu = conUsuRef.current.value;
+        const rolUsu = rolUsuRef.current.value;
+        
+        fetch("http://localhost:8080/usuario/guardar", {
+            headers: { "content-type": "application/json" },
+            method: "POST",
+            body: JSON.stringify({ "numero_identificacion": nidUsu, "nombres": nomUsu, "apellidos": apeUsu, "usuario": usuUsu, "contrasena": conUsu, "rol": rolUsu })
+        }).then(res => res.json())
+            .then(res => {
+                // setRefresh(!refresh);
+                alert(res.msg)
+            })
+    }
+    
     return (
+    
         <Fragment>
             <div className="container">
-                <div className="row align-items-start"></div>
+                <div className="row align-items-start">
                     <div className="col">
                     <form action="">
+                        
                         <div className="mb-3">
                             <br></br>
                             <h5>Información personal</h5>
@@ -15,53 +48,57 @@ export default function UsuarioGestion() {
                             
                         <div className="mb-3">
                             <label for="identificacion" className="form-label">N° Identificación:</label>
-                            <input type="number" className="w-50 form-control" id="identificacion" placeholder="Numero de Identificación"/>
+                            <input ref={nidUsuRef} type="number" className="w-50 form-control" id="identificacion" placeholder="Numero de Identificación"/>
                         </div>
+                        
                         <div className="mb-3">
                             <label for="nombres" className="form-label">Nombres:</label>
-                            <input type="text" className="w-50 form-control" id="nombres" placeholder="Ingrese nombres"required=""/>
+                            <input ref={nomUsuRef} type="text" className="w-50 form-control" id="nombres" placeholder="Ingrese nombres"required=""/>
                         </div>
+                        
                         <div className="mb-3">
                             <label for="apellidos" className="form-label">Apellidos:</label>
-                            <input type="text" className="w-50 form-control" id="apellidos" placeholder="Ingrese apellidos"required=""/>
+                            <input ref={apeUsuRef} type="text" className="w-50 form-control" id="apellidos" placeholder="Ingrese apellidos"required=""/>
                         </div>
-                            <br></br>
+                        <br></br>
                             
                         
-                            <div>
+                        <div>
                             <h5>Datos para ingreso al Sistema</h5>
-                            <div className="mb-3">
-                                <label for="usuario" className="form-label">Usuario:</label>
-                                <input type="email" className="w-50 form-control" id="user" placeholder="Registre un correo electronico, como nombre de usuario"required=""/>
-                            </div>
-                            <div className="mb-3">
-                                <label for="contrasena" className="form-label">Contraseña:</label>
-                                <input type="password" className="w-50 form-control" id="password" placeholder="Asignele una conraseña al usuario registrado"required=""/>
-                            </div>
+                        </div>
+                        
+                        <div className="mb-3">
+                            <label for="usuario" className="form-label">Usuario:</label>
+                            <input ref={usuUsuRef} type="email" className="w-50 form-control" id="user" placeholder="Registre un correo electronico, como nombre de usuario"required=""/>
+                        </div>
+                        
+                        <div className="mb-3">
+                            <label for="contrasena" className="form-label">Contraseña:</label>
+                            <input ref={conUsuRef} type="password" className="w-50 form-control" id="password" placeholder="Asignele una conraseña al usuario registrado"required=""/>
+                        </div>
+                        
+
+                        <div className="form-group">
+                            <label>Rol:</label>
+                            {/* </div><input ref={undRef} type="descripcion" className="w-50 form-control" placeholder=""/> */}
+                            <input ref={rolUsuRef} className="w-50 form-control" list="listRol" placeholder="" required=""/>
+                                <datalist id="listRol">
+                                    <option value="ADMINISTRATOR"></option>
+                                    <option value="PRODUCTION"></option>
+                                    <option value="INVENTORY"></option>
+                                </datalist>
+                            <br></br>
+                        </div>
                             
-                            <div className="mb-3"></div>
-                                <label for="sexi" className="form-label">Perfil del Usuario:</label>
-                                <br></br>
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
-                                    <label className="form-check-label" for="inlineRadio1">Administrator</label>
-                                </div>
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
-                                    <label className="form-check-label" for="inlineRadio2">Production </label>
-                                </div> 
-                                <div className="form-check form-check-inline">
-                                    <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
-                                    <label className="form-check-label" for="inlineRadio2">Inventory</label>
-                                </div> 
-                            </div>
-                            <br />
-                            <div className="d-grid gap-2 d-md-block">
-                                <button className="btn btn-success" type="button">Guardar</button>
-                            </div>
-                        </form>   
-                    </div>   
-                </div>      
+                           
+                        <br />
+                        <div className="d-grid gap-2 d-md-block">
+                        <button className="btn btn-primary" type="button" onClick = {guardarUsuario}>Guardar</button> <a> </a>
+                        </div>
+                    </form>   
+                    </div>  
+                </div> 
+            </div>      
             
         </Fragment>
     );
